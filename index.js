d74b72a7b2a0b7de3980a3d2262547234c046ad4
Revine <rempowa@gmx.fr>
2021-07-16 15:26:59 +0200

    Add cors

--- index.js
+++ index.js
@@ -1,4 +1,20 @@
 const app = require("./server");
+const cors = require("cors");
+
+const allowedDomains = ["http://localhost:3000"];
+
+app.use(
+  cors({
+    origin: function (origin, callback) {
+      if (!origin) return callback(null, true);
+
+      if (allowedDomains.indexOf(origin) === -1) {
+        return callback(new Error("Access Forbidden"), false);
+      }
+      return callback(null, true);
+    },
+  })
+);
 
 app.listen(process.env.PORT || 3100, () => {
   console.log("Server started on http://localhost:3100");
